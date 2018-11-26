5da1a4754b7dc0547eadc46ef15dcff8ba96da8a
RossPin <dorpetz@hotmail.com>
2018-11-26 15:54:40 +1300

    functions get recipes from db

---
+++ server/db/recipes.js
@@ -0,0 +1,30 @@
+const db = require('./connection')
+
+function getRecipes() {
+  return db('recipes')
+}
+
+function getRecipe (id) {
+  return db('recipes')
+  .where('id', id)
+  .first()
+}
+
+function getIngredients (recipe_id) {
+  return db('ingredients')
+    .where('recipe_id', recipe_id)  
+    .join('products', 'ingredients.product_id', 'products.id')
+    .select('product_id', 'name', 'qty', 'unit', 'cal', 'fat', 'sat_fat', 'protien', 'carb', 'sugar', 'avg_weight', 'density')    
+}
+
+function getSteps (recipe_id) {
+  return db('steps')
+    .where('recipe_id', recipe_id)
+}
+
+module.exports = {
+  getRecipes,
+  getRecipe,
+  getIngredients,
+  getSteps
+}
