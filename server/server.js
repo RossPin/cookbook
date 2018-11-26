335a3cefc3987b12b31c34b85c2eb760f3ac435e
RossPin <dorpetz@hotmail.com>
2018-11-26 15:54:08 +1300

    use recipes route

--- server/server.js
+++ server/server.js
@@ -2,10 +2,12 @@ const express = require('express')
 const bodyParser = require('body-parser')
 const server = express()
 const users = require('./routes/users')
+const recipes = require('./routes/recipes')
 const path = require('path')
 
 server.use(bodyParser.json())
 server.use('/api/users', users)
+server.use('/api/recipes', recipes)
 server.use(express.static(path.join(__dirname, '../public')))
 
 module.exports = server
