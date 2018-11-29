30d5c6a606990e47e1da392e82cb1577f2633358
RossPin <dorpetz@hotmail.com>
2018-11-29 15:31:35 +1300

    fix onChange

--- client/components/ProductAdd.jsx
+++ client/components/ProductAdd.jsx
@@ -14,7 +14,7 @@ class ProductAdd extends React.Component{
     super(props)
     this.state = {
       name: '',
-      group: '',
+      group: 'Meat and Eggs',
       cal: '',
       fat: '',
       sat_fat: '',
@@ -47,29 +47,29 @@ class ProductAdd extends React.Component{
   render(){
     return (
       <div>
-        <form submit={this.post}>
+        <form onSubmit={this.post}>
           <label htmlFor="group">Group</label><br/>
           <select onchange={this.update} name="group" id="group">            
             {groupOptions.map((group, i) => <option value={group} selected={this.state.group === group}>{group}</option>)}
           </select><br/>
-          <label htmlFor="name"></label><br/>
-          <input type="text" name="name" id="name" onchange={this.update}/><br/>
-          <label htmlFor="cal"></label><br/>
-          <input type="text" name="cal" id="cal" onchange={this.update}/><br/>
-          <label htmlFor="fat"></label><br/>
-          <input type="text" name="fat" id="fat" onchange={this.update}/><br/>
-          <label htmlFor="sat_fat"></label><br/>
-          <input type="text" name="sat_fat" id="sat_fat" onchange={this.update}/><br/>
-          <label htmlFor="protien"></label><br/>
-          <input type="text" name="protien" id="protien" onchange={this.update}/><br/>
-          <label htmlFor="carb"></label><br/>
-          <input type="text" name="carb" id="carb" onchange={this.update}/><br/>
-          <label htmlFor="sugar"></label><br/>
-          <input type="text" name="sugar" id="sugar" onchange={this.update}/><br/>
-          <label htmlFor="avg_weight"></label><br/>
-          <input type="text" name="avg-weight" id="avg-weight" onchange={this.update}/><br/>
-          <label htmlFor="density"></label><br/>
-          <input type="text" name="density" id="density" onchange={this.update}/><br/>
+          <label htmlFor="name">Product name</label><br/>
+          <input type="text" name="name" id="name" onChange={this.update}/><br/>
+          <label htmlFor="cal">Calories per 100g</label><br/>
+          <input type="text" name="cal" id="cal" onChange={this.update}/><br/>
+          <label htmlFor="fat">Fat per 100g</label><br/>
+          <input type="text" name="fat" id="fat" onChange={this.update}/><br/>
+          <label htmlFor="sat_fat">Saturated fat per 100g</label><br/>
+          <input type="text" name="sat_fat" id="sat_fat" onChange={this.update}/><br/>
+          <label htmlFor="protien">Protien per 100g</label><br/>
+          <input type="text" name="protien" id="protien" onChange={this.update}/><br/>
+          <label htmlFor="carb">Carbohydrates per 100g</label><br/>
+          <input type="text" name="carb" id="carb" onChange={this.update}/><br/>
+          <label htmlFor="sugar">Sugar per 100g</label><br/>
+          <input type="text" name="sugar" id="sugar" onChange={this.update}/><br/>
+          <label htmlFor="avg_weight">Average Weight if itemised</label><br/>
+          <input type="text" name="avg-weight" id="avg-weight" onChange={this.update}/><br/>
+          <label htmlFor="density">Density</label><br/>
+          <input type="text" name="density" id="density" onChange={this.update}/><br/>
           <input type="submit" value="Add Product"/>
         </form>
       </div>
