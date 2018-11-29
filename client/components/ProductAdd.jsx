6f46b7a93e485cb9cd925290dbc401deb11d6727
RossPin <dorpetz@hotmail.com>
2018-11-29 16:49:52 +1300

    on edit pre fill form with existiong values

--- client/components/ProductAdd.jsx
+++ client/components/ProductAdd.jsx
@@ -1,4 +1,5 @@
 import React from 'react'
+import {connect} from 'react-redux'
 import request from '../utils/api'
 
 const groupOptions = [
@@ -31,9 +32,10 @@ class ProductAdd extends React.Component{
   componentDidMount(){
     !this.props.auth.isAuthenticated && this.props.history.push('/')    
     const id = this.props.match.params.id
-    id && request('post', `recipes/product/${id}`).then(res => {
+    id && request('get', `recipes/product/${id}`).then(res => {
+      console.log(res.body)
       const {name, group, cal, fat, sat_fat, protien, carb, sugar, avg_weight, density} = res.body
-      this.setState({name, group, cal, fat, sat_fat, protien, carb, sugar, avg_weight, density})
+      this.setState({name, group, cal, fat, sat_fat, protien, carb, sugar, avg_weight: avg_weight || '', density: density || ''})
     })
   }
 
@@ -49,7 +51,9 @@ class ProductAdd extends React.Component{
     const id = this.props.match.params.id
     const state = this.state
     Object.keys(state).forEach(key => {
-      if (key !== 'name' && key !== 'group') state[key] = Number(state[key])
+      if (key !== 'name' && key !== 'group') {
+        if (state[key] === '') state[key] = null
+        else state[key] = Number(state[key])}
     })
     console.log(state)
     if (id) request('put', `recipes/product/${id}`, state).then(res => {
@@ -65,27 +69,27 @@ class ProductAdd extends React.Component{
       <div>
         <form onSubmit={this.post}>
           <label htmlFor="group">Group</label><br/>
-          <select onChange={this.update} name="group" id="group">            
-            {groupOptions.map((group, i) => <option key={i} value={group}>{group}</option>)}
+          <select onChange={this.update} name="group" id="group">                       
+            {groupOptions.map((group, i) => <option key={i} value={group} selected={this.state.group === group}>{group}</option>)}
           </select><br/>
           <label htmlFor="name">Product name</label><br/>
-          <input type="text" name="name" id="name" onChange={this.update}/><br/>
+          <input type="text" name="name" id="name" onChange={this.update} value={this.state.name}/><br/>
           <label htmlFor="cal">Calories per 100g</label><br/>
-          <input type="text" name="cal" id="cal" onChange={this.update}/><br/>
+          <input type="text" name="cal" id="cal" onChange={this.update} value={this.state.cal}/><br/>
           <label htmlFor="fat">Fat per 100g</label><br/>
-          <input type="text" name="fat" id="fat" onChange={this.update}/><br/>
+          <input type="text" name="fat" id="fat" onChange={this.update} value={this.state.fat}/><br/>
           <label htmlFor="sat_fat">Saturated fat per 100g</label><br/>
-          <input type="text" name="sat_fat" id="sat_fat" onChange={this.update}/><br/>
+          <input type="text" name="sat_fat" id="sat_fat" onChange={this.update} value={this.state.sat_fat}/><br/>
           <label htmlFor="protien">Protien per 100g</label><br/>
-          <input type="text" name="protien" id="protien" onChange={this.update}/><br/>
+          <input type="text" name="protien" id="protien" onChange={this.update} value={this.state.protien}/><br/>
           <label htmlFor="carb">Carbohydrates per 100g</label><br/>
-          <input type="text" name="carb" id="carb" onChange={this.update}/><br/>
+          <input type="text" name="carb" id="carb" onChange={this.update} value={this.state.carb}/><br/>
           <label htmlFor="sugar">Sugar per 100g</label><br/>
-          <input type="text" name="sugar" id="sugar" onChange={this.update}/><br/>
+          <input type="text" name="sugar" id="sugar" onChange={this.update} value={this.state.sugar}/><br/>
           <label htmlFor="avg_weight">Average Weight if itemised</label><br/>
-          <input type="text" name="avg-weight" id="avg-weight" onChange={this.update}/><br/>
+          <input type="text" name="avg-weight" id="avg-weight" onChange={this.update} value={this.state.avg_weight}/><br/>
           <label htmlFor="density">Density</label><br/>
-          <input type="text" name="density" id="density" onChange={this.update}/><br/>
+          <input type="text" name="density" id="density" onChange={this.update} value={this.state.density}/><br/>
           <input type="submit" value="Add Product"/>
         </form>
       </div>
@@ -93,4 +97,8 @@ class ProductAdd extends React.Component{
   }
 }
 
-export default ProductAdd
+const mapStateToProps = ({ auth }) => {
+  return { auth }
+}
+
+export default connect(mapStateToProps)(ProductAdd)
